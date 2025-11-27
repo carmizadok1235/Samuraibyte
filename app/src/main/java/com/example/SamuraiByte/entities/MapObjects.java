33d6e026c6f03c894129c8b9467cb781d817fbf8
Carmi Zadok <carmish144@gmail.com>
2025-11-27 22:39:42 +0200

    Adding pipes and invisible blocks to prevent player from walking on the trees.

--- app/src/main/java/com/example/SamuraiByte/entities/MapObjects.java
+++ app/src/main/java/com/example/SamuraiByte/entities/MapObjects.java
@@ -15,7 +15,20 @@ public enum MapObjects implements BitmapMethods {
     MEDIUM_STONE1(0,0,32,32, 20,6, R.drawable.stone_tileset),
     MEDIUM_STONE2(0,32,32,32, 0,0,R.drawable.stone_tileset),
     LARGE_STONE(32,0,64,48, 22,20,R.drawable.stone_tileset),
-    LONG_STONE(80,0,32,48, 0,0,R.drawable.stone_tileset);
+    LONG_STONE(80,0,32,48, 0,0,R.drawable.stone_tileset),
+    INVISIBLE_BLOCK1(0,0, 50, 1, 0, 41, R.drawable.pure_png),
+    INVISIBLE_BLOCK2(0,0, 50, 1, 0, 46, R.drawable.pure_png),
+    INVISIBLE_BLOCK3(0,0, 50, 1, 0, 6, R.drawable.pure_png),
+    LEFT_EDGE_PIPE(176, 0, 16, 16, 5, 7, R.drawable.pipes_spritesheet),
+    HORIZONTAL_PIPE(192, 0, 16, 16, 5, 7, R.drawable.pipes_spritesheet),
+    RIGHT_EDGE_PIPE(208, 0, 16, 16, 5, 7, R.drawable.pipes_spritesheet),
+    TOP_RIGHT_CONNECTOR_PIPE(192, 16, 16, 16, 5, 11, R.drawable.pipes_spritesheet),
+    TOP_LEFT_CONNECTOR_PIPE(176, 16, 16, 16, 5, 11, R.drawable.pipes_spritesheet),
+    BOTTOM_LEFT_CONNECTOR_PIPE(176, 32, 16, 16, 5, 11, R.drawable.pipes_spritesheet),
+    BOTTOM_RIGHT_CONNECTOR_PIPE(192, 32, 16, 16, 5, 11, R.drawable.pipes_spritesheet),
+    TOP_EDGE_PIPE(160, 0, 16, 16, 5, 7, R.drawable.pipes_spritesheet),
+    DIAGONAL_PIPE(160, 16, 16, 16, 5, 11, R.drawable.pipes_spritesheet),
+    BOTTOM_EDGE_PIPE(160, 32, 16, 16, 5, 11, R.drawable.pipes_spritesheet);
 
 
     Bitmap objectImage;
--- app/src/main/java/com/example/SamuraiByte/environments/GameMap.java
+++ app/src/main/java/com/example/SamuraiByte/environments/GameMap.java
@@ -27,13 +27,93 @@ public class GameMap {
     }
     public void initMap() {
         this.initTrees();
-//        this.initStones();
+        this.initInvisibleBlocks();
         this.mapObjectArrayList.add(new MapObject(new PointF(1200,900), MapObjects.HOUSE2));
         this.mapObjectArrayList.add(new MapObject(new PointF(3000,1200), MapObjects.HOUSE2));
         this.mapObjectArrayList.add(new MapObject(new PointF(1100,2300), MapObjects.LARGE_STONE));
         this.mapObjectArrayList.add(new MapObject(new PointF(1600,2600), MapObjects.MEDIUM_STONE1));
         this.mapObjectArrayList.add(new MapObject(new PointF(1400,2800), MapObjects.MEDIUM_STONE1));
         this.initCharacters();
+    }
+
+    private void initInvisibleBlocks() {
+        // Top-Left
+        this.mapObjectArrayList.add(new MapObject(new PointF(1240,0), MapObjects.INVISIBLE_BLOCK1));
+        this.mapObjectArrayList.add(new MapObject(new PointF(950,275), MapObjects.INVISIBLE_BLOCK2));
+        this.mapObjectArrayList.add(new MapObject(new PointF(850,550), MapObjects.INVISIBLE_BLOCK3));
+        this.mapObjectArrayList.add(new MapObject(new PointF(565,450), MapObjects.INVISIBLE_BLOCK2));
+        this.mapObjectArrayList.add(new MapObject(new PointF(465,725), MapObjects.INVISIBLE_BLOCK3));
+        this.mapObjectArrayList.add(new MapObject(new PointF(180,630), MapObjects.INVISIBLE_BLOCK2));
+        this.mapObjectArrayList.add(new MapObject(new PointF(80,905), MapObjects.INVISIBLE_BLOCK3));
+        this.mapObjectArrayList.add(new MapObject(new PointF(0,905), MapObjects.INVISIBLE_BLOCK3));
+
+        // Top-Right
+        this.mapObjectArrayList.add(new MapObject(new PointF(2976,0), MapObjects.INVISIBLE_BLOCK1));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3264,275), MapObjects.INVISIBLE_BLOCK2));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3372,550), MapObjects.INVISIBLE_BLOCK3));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3648,450), MapObjects.INVISIBLE_BLOCK2));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3756,725), MapObjects.INVISIBLE_BLOCK3));
+        this.mapObjectArrayList.add(new MapObject(new PointF(4032,630), MapObjects.INVISIBLE_BLOCK2));
+        this.mapObjectArrayList.add(new MapObject(new PointF(4140,905), MapObjects.INVISIBLE_BLOCK3));
+        this.mapObjectArrayList.add(new MapObject(new PointF(4230,905), MapObjects.INVISIBLE_BLOCK3));
+
+        // Bottom-Left
+        this.mapObjectArrayList.add(new MapObject(new PointF(0, 3045), MapObjects.LEFT_EDGE_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(96, 3045), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(192, 3045), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(288, 3045), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(384, 3045), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(480, 3045), MapObjects.TOP_RIGHT_CONNECTOR_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(480, 3141), MapObjects.DIAGONAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(480, 3237), MapObjects.BOTTOM_LEFT_CONNECTOR_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(576, 3237), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(672, 3237), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(768, 3237), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(864, 3237), MapObjects.TOP_RIGHT_CONNECTOR_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(864, 3333), MapObjects.BOTTOM_LEFT_CONNECTOR_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(960, 3333), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(1056, 3333), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(1152, 3333), MapObjects.TOP_RIGHT_CONNECTOR_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(1152, 3429), MapObjects.BOTTOM_LEFT_CONNECTOR_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(1248, 3429), MapObjects.TOP_RIGHT_CONNECTOR_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(1248, 3525), MapObjects.DIAGONAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(1248, 3621), MapObjects.DIAGONAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(1248, 3717), MapObjects.BOTTOM_LEFT_CONNECTOR_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(1344, 3717), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(1440, 3717), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(1536, 3717), MapObjects.TOP_RIGHT_CONNECTOR_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(1536, 3816), MapObjects.DIAGONAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(1536, 3909), MapObjects.DIAGONAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(1536, 4005), MapObjects.BOTTOM_EDGE_PIPE));
+
+        // Bottom-Right
+        this.mapObjectArrayList.add(new MapObject(new PointF(4416, 3045), MapObjects.RIGHT_EDGE_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(4320, 3045), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(4224, 3045), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(4128, 3045), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(4032, 3045), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3936, 3045), MapObjects.TOP_LEFT_CONNECTOR_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3936, 3141), MapObjects.DIAGONAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3936, 3237), MapObjects.BOTTOM_RIGHT_CONNECTOR_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3840, 3237), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3744, 3237), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3648, 3237), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3552, 3237), MapObjects.TOP_LEFT_CONNECTOR_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3552, 3333), MapObjects.BOTTOM_RIGHT_CONNECTOR_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3456, 3333), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3360, 3333), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3264, 3333), MapObjects.TOP_LEFT_CONNECTOR_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3264, 3429), MapObjects.BOTTOM_RIGHT_CONNECTOR_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3168, 3429), MapObjects.TOP_LEFT_CONNECTOR_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3168, 3525), MapObjects.DIAGONAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3168, 3621), MapObjects.DIAGONAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3168, 3717), MapObjects.BOTTOM_RIGHT_CONNECTOR_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(3072, 3717), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(2976, 3717), MapObjects.HORIZONTAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(2880, 3717), MapObjects.TOP_LEFT_CONNECTOR_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(2880, 3816), MapObjects.DIAGONAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(2880, 3909), MapObjects.DIAGONAL_PIPE));
+        this.mapObjectArrayList.add(new MapObject(new PointF(2880, 4005), MapObjects.BOTTOM_EDGE_PIPE));
     }
 
     private void initCharacters() {
